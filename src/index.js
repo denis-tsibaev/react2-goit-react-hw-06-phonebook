a1214445a1894ba8e6bce74eb5d60f4ec6e85347
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-12-12 22:08:50 +0700

    tmp pause

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+import 'modern-normalize/modern-normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import 'modern-normalize/modern-normalize.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
